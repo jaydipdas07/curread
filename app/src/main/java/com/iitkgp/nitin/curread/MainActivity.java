d76fc81ce2e40a0e58d31f9ec07a7e2dd317ca5f
nitinkgp23 <nitin.iitkgp23@gmail.com>
2016-10-29 04:30:17 +0530

    Initial commit

---
+++ app/src/main/java/com/iitkgp/nitin/curread/MainActivity.java
@@ -0,0 +1,170 @@
+package com.iitkgp.nitin.curread;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvmain;
+    Button b1;
+    public ImageView mImageView;
+    private Bitmap mImageBitmap;
+    Mat imageMat;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    String mCurrentPhotoPath;
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    Log.i("OpenCV", "OpenCV loaded successfully");
+                    imageMat=new Mat();
+                } break;
+                default:
+                {
+                    super.onManagerConnected(status);
+                } break;
+            }
+        }
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvmain = (TextView) findViewById(R.id.maintext);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.myanimation);
+        tvmain.startAnimation(animation);
+
+        b1 = (Button) findViewById(R.id.buttonmain);
+        mImageView = (ImageView)findViewById(R.id.imageView);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+
+    }
+
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                // Create the File where the photo should go
+                File photoFile = null;
+                try {
+                    photoFile = createImageFile();
+                } catch (IOException ex) {
+                    // Error occurred while creating the File
+                }
+                // Continue only if the File was successfully created
+                if (photoFile != null) {
+                    Uri photoURI = FileProvider.getUriForFile(this,
+                            "com.example.android.fileprovider",
+                            photoFile);
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+
+                    Bitmap b = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                    mImageView.setImageBitmap(b);
+                    //loadImageFromStorage(photoFile.getAbsolutePath(), photoFile.getName());
+                }
+            }
+        }
+
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
+    }
+/*
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            mImageBitmap = (Bitmap) extras.get("data");
+            editPicture(mImageBitmap);
+        }
+    } */
+    private void editPicture(Bitmap image){
+      //  imageMat = new Mat(image.getHeight(),image.getWidth(), CvType.CV_8UC1);
+      //  Utils.bitmapToMat(image, imageMat);
+
+
+//Do smth.
+      // Imgproc.cvtColor(imageMat, imageMat, Imgproc.COLOR_RGB2GRAY,4);
+
+//Then convert the processed Mat to Bitmap
+       //  Bitmap resultBitmap = Bitmap.createBitmap(imageMat.cols(),  imageMat.rows(),Bitmap.Config.ARGB_8888);;
+       //  Utils.matToBitmap(imageMat, resultBitmap);
+
+//Set member to the Result Bitmap. This member is displayed in an ImageView
+        //mResult = resultBitmap;
+        mImageView.setImageBitmap(image);
+    }
+    public void onResume()
+    {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            Log.d("OpenCV", "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
+        } else {
+            Log.d("OpenCV", "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+   /* private void loadImageFromStorage(String path, String name)
+    {
+
+        try {
+            File f=new File(path, name);
+            Bitmap b = BitmapFactory.decodeStream(new FileInputStream(f));
+            //ImageView img=(ImageView)findViewById(R.id.imageView);
+            //img.setImageBitmap(b);
+            editPicture(b);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }*/
+}
